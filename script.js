512982008ad6119943f3b39c820659e8cd136ebe
Jessenth Ebenezer <30726289+sm1else-bot@users.noreply.github.com>
2024-11-05 18:30:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -39,7 +39,6 @@ function checkRegistration() {
             <p>Name: ${vehicle.name}</p>
             <p>Number Plate: ${vehicle['number plate']}</p>
             <p>Company: ${vehicle.company}</p>
-            // <p>Purpose: ${vehicle.purpose}</p>
             <hr>
         `).join('');
     } else {
